a247c76b0175623d937d572406820357b27d270d
sc0der <siriusproger@gmail.com>
2021-12-10 23:22:43 +0500

    created second interface

--- ABC_factory.ts
+++ ABC_factory.ts
@@ -1,3 +1,18 @@
 interface AbstractProductA {
     usefulFunctionA(): string;
 }
+
+interface AbstractProductB {
+    /**
+     * Продукт B способен работать самостоятельно...
+     */
+    usefulFunctionB(): string;
+
+    /**
+     * ...а также взаимодействовать с Продуктами A той же вариации.
+     *
+     * Абстрактная Фабрика гарантирует, что все продукты, которые она создает,
+     * имеют одинаковую вариацию и, следовательно, совместимы.
+     */
+    anotherUsefulFunctionB(collaborator: AbstractProductA): string;
+}
