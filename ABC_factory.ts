0cf6bed81bdc0021e930b76c2f64657af68eb792
sc0der <siriusproger@gmail.com>
2021-12-10 23:24:09 +0500

    created first concrete product 2

--- ABC_factory.ts
+++ ABC_factory.ts
@@ -29,3 +29,38 @@ class ConcreteProductA2 implements AbstractProductA {
         return 'The result of the product A2.';
     }
 }
+
+
+class ConcreteProductB1 implements AbstractProductB {
+
+    public usefulFunctionB(): string {
+        return 'The result of the product B1.';
+    }
+
+    /**
+     * Продукт B1 может корректно работать только с Продуктом A1. Тем не менее,
+     * он принимает любой экземпляр Абстрактного Продукта А в качестве
+     * аргумента.
+     */
+    public anotherUsefulFunctionB(collaborator: AbstractProductA): string {
+        const result = collaborator.usefulFunctionA();
+        return `The result of the B1 collaborating with the (${result})`;
+    }
+}
+
+class ConcreteProductB2 implements AbstractProductB {
+
+    public usefulFunctionB(): string {
+        return 'The result of the product B2.';
+    }
+
+    /**
+     * Продукт B2 может корректно работать только с Продуктом A2. Тем не менее,
+     * он принимает любой экземпляр Абстрактного Продукта А в качестве
+     * аргумента.
+     */
+    public anotherUsefulFunctionB(collaborator: AbstractProductA): string {
+        const result = collaborator.usefulFunctionA();
+        return `The result of the B2 collaborating with the (${result})`;
+    }
+}
